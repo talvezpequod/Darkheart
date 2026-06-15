ef200247f4d2a6ea91bf914277ad48e7e0f2ea7e
talvezpequod <talvezpequod@gmail.com>
2026-06-15 09:23:51 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Peeeequod",
+        name: "Irritando a Pequod :)",
         // Activity type number (0 = Playing).
         type: 0, 
       },
